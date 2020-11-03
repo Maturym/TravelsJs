a09d0a648cc0bed57590b0fa2305e717f26279c7
Elizabeth <whosingsin@gmail.com>
2020-11-04 02:48:27 +0300

    fixed scripts:

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+// const reportCards = document.querySelector(".report__cards");
+// const nextButton = document.querySelector('.report-card--next');
+
+
+// const lastChild = reportCards.lastElementChild;
+
+// console.log(lastChild);
+
+// const sliderHandler = () =>{
+
+//   const currentEl = document.querySelector('.current');
+
+//   currentEl.fadeToggle(1000, ()=> {
+//     this.next
+//   });
+// };
+
+// nextButton.addEventListener('click', sliderHandler);
